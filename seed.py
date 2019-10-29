739f99ea9fdfaeb70536be7a0b5474530afba9b0
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-29 13:03:18 -0700

    WIP: Load movies function

--- seed.py
+++ seed.py
@@ -1,12 +1,14 @@
 """Utility file to seed ratings database from MovieLens data in seed_data/"""
 
 from sqlalchemy import func
-from model import User
+from model import User, Movie
 # from model import Rating
 # from model import Movie
 
 from model import connect_to_db, db
 from server import app
+
+from datetime import datetime
 
 
 def load_users():
@@ -36,6 +38,35 @@ def load_users():
 
 def load_movies():
     """Load movies from u.item into database."""
+
+    print("Movies")
+
+    Movie.query.delete()
+
+    for row in open("seed_data/u.item"):
+        row = row.rstrip()
+        movie_info = row.split("|")
+
+        movie_id = movie_info[0]
+
+        #removing year at end of movie title
+        title = movie_info[1]
+        #ERROR! GETTING YEAR STILL!!! 
+        title_name = title.split(" ")
+        " ".join(title_name.pop())
+
+        released_at = movie_info[2]
+        format = "%d-%b-%Y"
+        released_at = datetime.strptime(released_at, format)
+
+        imdb_url = movie_info[4]
+
+        movie = Movie(movie_id=movie_id, title=title, released_at=released_at, imdb_url=imdb_url)
+
+        db.session.add(movie)
+
+    db.session.commit()
+
 
 
 def load_ratings():
